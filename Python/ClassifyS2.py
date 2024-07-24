a7aca8831b5f3ef871fd7ad949fe3100050c5ea5
DanGeospatial <daniel.nelson.ca@gmail.com>
2024-07-23 21:57:20 -0400

    fix licenses

--- Python/ClassifyS2.py
+++ Python/ClassifyS2.py
@@ -13,3 +13,25 @@
    See the License for the specific language governing permissions and
    limitations under the License.
 """
+
+import ee
+import eemont
+
+import ray
+from ray import tune
+from ray.tune.search.hyperopt import HyperOptSearch
+from hyperopt import hp
+
+# Initialize Earth Engine Api
+# More information at developers.google.com/earth-engine/guides/python_install-conda#windows
+ee.Initialize()
+
+# ______________________________________________________________________________
+# Load EE Assets
+# ______________________________________________________________________________
+
+newpolygon = ee.FeatureCollection("users/danielnelsonca/UndergradThesis/newpolygon")
+Yukon_points_merged = ee.FeatureCollection("users/danielnelsonca/UndergradThesis/Yukon_points_merged")
+TWI4 = ee.Image("users/danielnelsonca/UndergradThesis/Topographic_Wetness_Index_v4")
+TPI2 = ee.Image("users/danielnelsonca/UndergradThesis/Topographic_Position_Index_v2")
+
